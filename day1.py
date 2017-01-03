66d3bad495e2c905a6a73e578160b14bb0f07ddd
thesharpshooter <prakash9266@gmail.com>
2017-01-04 00:08:19 +0530

    Added time stamp

--- day1.py
+++ day1.py
@@ -1,7 +1,12 @@
+from time import time
+
 def find_max(array):
+    start_time = time()
     temp = array[0]
     for x in array:
         if x >= temp:
             temp = x
+    end_time = time()
+    print (end_time - start_time)
     return temp
 
