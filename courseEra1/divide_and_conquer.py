477510d13408c58f2ad75992b04dc23383863b5f
thesharpshooter <prakash9266@gmail.com>
2017-01-20 05:36:10 +0530

    Exhausted

--- courseEra1/divide_and_conquer.py
+++ courseEra1/divide_and_conquer.py
@@ -1,3 +1,5 @@
+import math
+
 # Multiply two number
 
 
@@ -17,10 +19,50 @@ def product(a, b):
 
     return p1*pow(10, n1)+(p3-p1-p2)*pow(10, n1//2)+p2
 
-a = input("Wanna try more?(Y/N)")
-while(a == 'Y'):
-    num1 = int(input("Input num1"))
-    num2 = int(input("Number2"))
-    print product(num1,num2)
-    a = input("Wanna try more")
 
+# closest pair
+
+# RETURNS DISTANCE BETWEEN TWO POINTS
+
+
+def dis(A, B):
+    return math.sqrt((A[0]-B[0])**2+(A[1]-B[1])**2)
+
+
+def closestPair(P):
+    n = len(P)
+    # IF LESS NO OF POINTS SIMPLY RETURN THEIR DISTANCE
+    if n <= 3:
+        d = 100000*100
+        for i in range(n):
+            for j in range(i+1, n):
+                if dis(P[i], P[j]) < d:
+                    d = dis(P[i], P[j])
+        return d
+
+    # SORT THE POINTS ACCORDING TO THEIR X COORDINATE
+    Px = sorted(P)
+    mid = Px[n//2]
+    L = Px[:n//2]
+    R = Px[n//2:]
+
+    # MINIMUM OF THE TWO MINIMUM DISTANCE IS TAKEN
+    dl = closestPair(L)
+    dr = closestPair(R)
+    d = min(dl, dr)
+
+    # STRIP TAKE POINTS THAT ARE WITHIN THE RANGE OF 2DELTA(d)
+    strip = []
+    for p in P:
+        if abs(p[0]-mid[0]) <= d and abs(p[1]-mid[1]) <= d and p != mid:
+            strip.append(p)
+
+    ns = len(strip)
+    for j in range(ns):
+        for k in range(j+1, ns):
+            if dis(strip[j], strip[k]) < d:
+                d = dis(strip[j], strip[k])
+    return d
+
+array = [[3, -1], [2, 3], [12, 30], [40, 50], [5, 1], [12, 10], [3, 4]]
+print closestPair(array)
